2b12c2ceb256733c494907eda62a2a94f1215cd8
KeoiMadBro <davide.donato8@gmail.com>
2022-02-17 19:40:19 +0100

    First commit.

---
+++ Update IP Signum Node 60Sec.ahk
@@ -0,0 +1,5 @@
+﻿Loop{
+  Run "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -File "C:\File\Update IP Signum Node.ps1", , Min
+  Sleep, 60000
+}
+Return
---
+++ Update IP Signum Node.ps1
@@ -0,0 +1,21 @@
+$CurrentIP = (Invoke-WebRequest -uri "http://ifconfig.me/ip" -UseBasicParsing).Content
+
+$OldIP = Get-Content "<<Here put the path of the folder where the Signum Node is located>>\conf\node-default.properties" | Select-String -Pattern "P2P.myAddress = " | Select-Object -ExpandProperty Line
+
+$content = Get-Content "<<Here put the path of the folder where the Signum Node is located>>\conf\node-default.properties"
+
+# If the IP inside the config file is the same as the new one, nothing is gonna happen.
+If ( $OldIP -eq "P2P.myAddress = $CurrentIP" ) {
+}
+# If it's not, the current IP is replaced with the new one, then the Signum Node is stopped, and after 10 seconds is started.
+elseif ( $OldIP -ne "P2P.myAddress = $CurrentIP" ) {
+    $content | ForEach-Object { $_ -replace $OldIP, "P2P.myAddress = $CurrentIP" } | Set-Content "<<Here put the path of the folder where the Signum Node is located>>\conf\node-default.properties"
+    Stop-Process -Name javaw -Force
+    Start-Sleep -Milliseconds 10000
+    Start-Process -FilePath "<<Here put the path of the folder where the Signum Node is located>>\signum-node.exe"
+    
+    # The next 2 rows will create a .txt file which acts as a log file, with the date and time, then the entire P2P.myAddress string and the new IP address.
+    # Note, for security purposes it's not gonna keep more than 1 row, if you want to have more than one row, add one space and "-Append" after "-Force".
+    $DateTimeUpdate = Get-Date -Format "dddd dd/MMM/yyyy HH:mm:ss"
+    "$DateTimeUpdate / Old myAddress string -> $OldIP / New IP -> $CurrentIP" | Out-File -FilePath "C:\Users\$ENV:USERNAME\Desktop\IP changed.txt" -Force
+}
